0c55b8bec2f3dc718b86717980fe2d8c2d7d428e
Tobias Merkel <stylogey@gmail.com>
2023-06-22 18:52:41 +0200

    SystemUI: Refresh system icons on theme change
    System icon pack changes were previously managed by reconstructing
    some components which used them, such as KeyguardStatusBarView. This
    introduced a memory leak where KeyguardStatusBarViewController never
    unregistered a callback, thus it was not being garbage collected.
    Furthermore, some usages of the system icons were not updated on a
    pack change. Change that by recreating the icons on theme changes.
    
    This reverts commit db9671c8544cce1cb20dc5c439e8eea15df21ce0.
    
    Change-Id: Ic2b774d243546a88ac2e17f8441e073629caa1c8

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ui/StatusBarIconControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ui/StatusBarIconControllerImpl.java
@@ -508,6 +508,11 @@ public class StatusBarIconControllerImpl implements Tunable,
         return s;
     }
 
+    @Override
+    public void onThemeChanged() {
+        refreshIconGroups();
+    }
+
     private String createExternalSlotName(String slot) {
         if (slot.endsWith(EXTERNAL_SLOT_SUFFIX)) {
             return slot;
