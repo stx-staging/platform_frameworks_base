f94e958d5f24e227e1eb41f91f23123000e2d993
Anay Wadhera <anay1018@gmail.com>
2024-01-01 02:41:59 -0500

    SystemUI: Split global actions impl into its own module from PluginModule
    This will allow us to create our own Global Actions implementation.
    
    Change-Id: I5bb88bc1bacfae719dfe1887c0b943746ced5662

--- packages/SystemUI/src/com/android/systemui/dagger/ReferenceSystemUIModule.java
+++ packages/SystemUI/src/com/android/systemui/dagger/ReferenceSystemUIModule.java
@@ -36,12 +36,14 @@ import com.android.systemui.dock.DockManagerImpl;
 import com.android.systemui.doze.DozeHost;
 import com.android.systemui.education.dagger.ContextualEducationModule;
 import com.android.systemui.emergency.EmergencyGestureModule;
+import com.android.systemui.globalactions.GlobalActionsModule;
 import com.android.systemui.inputdevice.tutorial.KeyboardTouchpadTutorialModule;
 import com.android.systemui.keyboard.shortcut.ShortcutHelperModule;
 import com.android.systemui.keyguard.dagger.KeyguardModule;
 import com.android.systemui.keyguard.ui.composable.blueprint.DefaultBlueprintModule;
 import com.android.systemui.keyguard.ui.view.layout.blueprints.KeyguardBlueprintModule;
 import com.android.systemui.keyguard.ui.view.layout.sections.KeyguardSectionsModule;
+import com.android.systemui.globalactions.GlobalActionsModule;
 import com.android.systemui.media.dagger.MediaModule;
 import com.android.systemui.media.muteawait.MediaMuteAwaitConnectionCli;
 import com.android.systemui.media.nearby.NearbyMediaDevicesManager;
@@ -133,6 +135,7 @@ import javax.inject.Named;
         EmergencyGestureModule.class,
         FlashlightModule.class,
         GestureModule.class,
+        GlobalActionsModule.class,
         HeadsUpModule.class,
         KeyguardModule.class,
         KeyboardShortcutsModule.class,
