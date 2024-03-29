4c221f2277b8d3826cabb960a2122f559e1c4e0d
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-03-29 12:17:41 +0530

    PIHooks: Hide pixel sysconfig features from Google Photos
    Photos checks the device model in addition to system features for
    enabling unlimited photos backup. On non-pixels with the pixel
    feature sysconfigs, it misleadingly shows unlimited backup while
    actually counting towards storage.
    
    Change-Id: I300552bc84acf67c3f755a0803ddf8a736272857

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -143,6 +143,7 @@ import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.os.ApplicationSharedMemory;
 import com.android.internal.os.SomeArgs;
 import com.android.internal.pm.RoSystemFeatures;
+import com.android.internal.util.PropImitationHooks;
 import com.android.internal.util.UserIcons;
 
 import dalvik.system.VMRuntime;
@@ -858,7 +859,8 @@ public class ApplicationPackageManager extends PackageManager {
                 return maybeHasSystemFeature;
             }
         }
-        return mHasSystemFeatureCache.query(new HasSystemFeatureQuery(name, version));
+        return PropImitationHooks.hasSystemFeature(name,
+                mHasSystemFeatureCache.query(new HasSystemFeatureQuery(name, version)));
     }
 
     /** @hide */
